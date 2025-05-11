9345b32bd0ce0e406065d78f3588b93dfb96ccd8
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-11 20:35:53 +0530

    first commit

--- src/components/Pages/MyContactPage.js
+++ src/components/Pages/MyContactPage.js
@@ -29,7 +29,8 @@ const MyContactPage = () => {
     setLoading(true);
     // http://localhost:5000/contact
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/contact`, {
+      // const response = await fetch(`${process.env.REACT_APP_API_URL}/contact`, {
+      const response = await fetch('http://localhost:5000/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(values),
